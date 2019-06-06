036da9115bd6b4b992df15865f6a72d63c35fc8b
ika <ika@DESKTOP-24JR8AH>
2019-06-06 20:50:05 +0900

    up

---
+++ timechart/charPulldown.js
@@ -0,0 +1,232 @@
+// キャラプルダウン生成
+function getCharPulldown() {
+
+	var charArray = [ {
+		val : "akari",
+		txt : "アカリ"
+	}, {
+		val : "akino",
+		txt : "アキノ"
+	}, {
+		val : "ayane",
+		txt : "アヤネ"
+	}, {
+		val : "ayane_kurisumasu",
+		txt : "アヤネ（クリスマス）"
+	}, {
+		val : "ayumi",
+		txt : "アユミ"
+	}, {
+		val : "arisa",
+		txt : "アリサ"
+	}, {
+		val : "an",
+		txt : "アン"
+	}, {
+		val : "annna",
+		txt : "アンナ"
+	}, {
+		val : "io",
+		txt : "イオ"
+	}, {
+		val : "iriya",
+		txt : "イリヤ"
+	}, {
+		val : "eriko",
+		txt : "エリコ"
+	}, {
+		val : "eriko_barenntain",
+		txt : "エリコ（バレンタイン）"
+	}, {
+		val : "kaori",
+		txt : "カオリ"
+	}, {
+		val : "kasumi",
+		txt : "カスミ"
+	}, {
+		val : "kyaru",
+		txt : "キャル"
+	}, {
+		val : "kyaru_sama-",
+		txt : "キャル（サマー）"
+	}, {
+		val : "kyouka",
+		txt : "キョウカ"
+	}, {
+		val : "kuuka_o-edo",
+		txt : "クウカ（オーエド）"
+	}, {
+		val : "kurisu",
+		txt : "クリス"
+	}, {
+		val : "kurumi",
+		txt : "クルミ"
+	}, {
+		val : "kurumi_kurisumasu",
+		txt : "クルミ（クリスマス）"
+	}, {
+		val : "kokkoro",
+		txt : "コッコロ"
+	}, {
+		val : "kokkoro_sama-",
+		txt : "コッコロ（サマー）"
+	}, {
+		val : "saren",
+		txt : "サレン"
+	}, {
+		val : "ji-ta",
+		txt : "ジータ"
+	}, {
+		val : "siori",
+		txt : "シオリ"
+	}, {
+		val : "sizuru",
+		txt : "シズル"
+	}, {
+		val : "sizuru_barenntain",
+		txt : "シズル（バレンタイン）"
+	}, {
+		val : "sinobu",
+		txt : "シノブ"
+	}, {
+		val : "jun",
+		txt : "ジュン"
+	}, {
+		val : "suzuna",
+		txt : "スズナ"
+	}, {
+		val : "suzume",
+		txt : "スズメ（サマー）"
+	}, {
+		val : "tamaki",
+		txt : "タマキ"
+	}, {
+		val : "tamaki_sama-",
+		txt : "タマキ（サマー）"
+	}, {
+		val : "tika",
+		txt : "チカ"
+	}, {
+		val : "tika_kurisumasu",
+		txt : "チカ（クリスマス）"
+	}, {
+		val : "tumugi",
+		txt : "ツムギ"
+	}, {
+		val : "tomo",
+		txt : "トモ"
+	}, {
+		val : "nanaka",
+		txt : "ナナカ"
+	}, {
+		val : "ninon",
+		txt : "ニノン"
+	}, {
+		val : "ninon_o-edo",
+		txt : "ニノン（オーエド）"
+	}, {
+		val : "nozomi",
+		txt : "ノゾミ"
+	}, {
+		val : "hatune",
+		txt : "ハツネ"
+	}, {
+		val : "sinobu_harowin",
+		txt : "ハロシノ"
+	}, {
+		val : "miyako_harowin",
+		txt : "ハロプリ"
+	}, {
+		val : "misaki_harowin",
+		txt : "ハロミサ"
+	}, {
+		val : "hiyori",
+		txt : "ヒヨリ"
+	}, {
+		val : "hiyori_nyu-iya-",
+		txt : "ヒヨリ（ニューイヤー）"
+	}, {
+		val : "pekori-nu",
+		txt : "ペコリーヌ"
+	}, {
+		val : "pekori-nu_sama-",
+		txt : "ペコリーヌ（サマー）"
+	}, {
+		val : "makoto",
+		txt : "マコト"
+	}, {
+		val : "mahiru",
+		txt : "マヒル"
+	}, {
+		val : "maho",
+		txt : "マホ"
+	}, {
+		val : "misato",
+		txt : "ミサト"
+	}, {
+		val : "misogi",
+		txt : "ミソギ"
+	}, {
+		val : "mituki",
+		txt : "ミツキ"
+	}, {
+		val : "mihuyu",
+		txt : "ミフユ"
+	}, {
+		val : "mihuyu_sama-",
+		txt : "ミフユ（サマー）"
+	}, {
+		val : "mimi",
+		txt : "ミミ"
+	}, {
+		val : "miyako",
+		txt : "ミヤコ"
+	}, {
+		val : "muimi",
+		txt : "ムイミ"
+	}, {
+		val : "monika",
+		txt : "モニカ"
+	}, {
+		val : "yui",
+		txt : "ユイ"
+	}, {
+		val : "yui_nyu-iya-",
+		txt : "ユイ（ニューイヤー）"
+	}, {
+		val : "yukari",
+		txt : "ユカリ"
+	}, {
+		val : "yuki",
+		txt : "ユキ"
+	}, {
+		val : "rima",
+		txt : "リマ"
+	}, {
+		val : "rulu",
+		txt : "ルゥ"
+	}, {
+		val : "ruka",
+		txt : "ルカ"
+	}, {
+		val : "rei",
+		txt : "レイ"
+	}, {
+		val : "rei_nyu-iya-",
+		txt : "レイ（ニューイヤー）"
+	} ];
+
+	var selecter = $('#prcn-char').children().clone();
+
+	for (var i = 0; i < charArray.length; i++) {
+		var op = document.createElement("option");
+		op.value = charArray[i].val;
+		op.text = charArray[i].txt;
+
+		selecter.append(op);
+
+	}
+
+	return selecter;
+
+}
--- timechart/main.js
+++ timechart/main.js
@@ -1,15 +1,24 @@
-
-
-
 $(document).ready(function() {
 
 	let charLimit = 5;
 	var lineCount = 0;
 
-	$('.prcn-char').html($('#prcn-char').html());
+	// 初期化
+	$('.prcn-char').html(getCharPulldown());
 	$('.prcn-rank').html($('#prcn-rank').html());
 	$('.prcn-star').html($('#prcn-star').html());
 	$('.prcn-unique').html($('#prcn-unique').html());
+
+	$('.ui.dropdown').dropdown();
+
+	for (var i = 0; i < charLimit; i++) {
+		// 画像初期化
+		var selectElm = $('#char' + i + ' > div.prcn-char > div > select');
+		var imgElm = getCharImg(selectElm.val());
+		imgElm.css("width", "100%");
+		selectElm.parent().parent().parent().children('.prcn-img').html('');
+		selectElm.parent().parent().parent().children('.prcn-img').append(imgElm);
+	}
 
 	$('.button-init').click(function() {
 		$('.edit-area').html('');
@@ -56,9 +65,10 @@ $(document).ready(function() {
 		var result = '';
 
 		for (var i = 0; i < charLimit; i++) {
-			result += getCharName(i) + ' ';
+			result += getCharName(i);
 			result += getCharStar(i) + ' ';
-			result += getCharRank(i);
+			result += getCharRank(i) + ' ';
+			result += getCharUnique(i) != '-' ? '専用' + getCharUnique(i) : '';
 			result += '<br>';
 
 		}
@@ -87,61 +97,129 @@ $(document).ready(function() {
 			result += '<br>';
 		}
 
-		$('.export-area p').html(result);
+		$('.tl-export-area p').html(result);
 
 	});
 
 	$(document).on("click", '.button-copy', function() {
-		execCopy($('.export-area p').html());
+		execCopy($('.tl-export-area p').html());
 	});
 
-
-	$(document).on("click", '.test', function() {
-		$('#char0').children('.prcn-char').children('.input').children('input').val('キャラ0');
-		$('#char1').children('.prcn-char').children('.input').children('input').val('キャラ1');
-		$('#char2').children('.prcn-char').children('.input').children('input').val('キャラ2');
-		$('#char3').children('.prcn-char').children('.input').children('input').val('キャラ3');
-		$('#char4').children('.prcn-char').children('.input').children('input').val('キャラ4');
+	$(document).on("click", '.button-create-team-set', function() {
+		createTeamSet();
 	});
 
+	$(document).on("click", '.tsts', function() {
+	    let canvas = document.getElementById("canvas");
 
-	function getCharName(num) {
-		return $('#char' + num).children('.prcn-char').children('.input').children('input').val();
-	}
-	function getCharStar(num) {
-		return $('#char' + num).children('.prcn-star').children('select').val();
-	}
-	function getCharRank(num) {
-		return $('#char' + num).children('.prcn-rank').children('select').val();
-	}
-	function getlastTime() {
-		var result = $('.edit-area div:last-child').children('input[type="time"]').val();
-		if (result === undefined) {
-			return "01:30";
-		}
-		return result;
-	}
+	    let link = document.createElement("a");
+	    link.href = canvas.toDataURL("image/jpg");
+	    link.download = "test.png";
+	    link.click();
+	});
+
+	// キャラプルダウン変更時 画像更新
+	$('div.prcn-char > div > select').dropdown('setting', 'onChange', function() {
+		var imgElm = getCharImg($(this).val());
+		imgElm.css("width", "100%");
+		$(this).parent().parent().parent().children('.prcn-img').html('');
+		$(this).parent().parent().parent().children('.prcn-img').append(imgElm);
+	});
 
 });
 
-// タイムチャートコピー
-function execCopy(string){
-	  var tmp = document.createElement("div");
-	  var pre = document.createElement('pre');
-	  pre.style.webkitUserSelect = 'auto';
-	  pre.style.userSelect = 'auto';
-
-
-	  string = string.replace(/<br>/g, "\r\n");
-	  tmp.appendChild(pre).textContent = string;
-	  var s = tmp.style;
-	  s.position = 'fixed';
-	  s.right = '200%';
-
-	  document.body.appendChild(tmp);
-	  document.getSelection().selectAllChildren(tmp);
-	  var result = document.execCommand("copy");
-	  document.body.removeChild(tmp);
-
-	  return result;
+function getCharName(num) {
+	return $('#char' + num + ' > div.prcn-char > div > div.text').html();
+}
+function getCharStar(num) {
+	return $('#char' + num + ' > div.prcn-star > div > div.text').html();
+}
+function getCharRank(num) {
+	return $('#char' + num + ' > div.prcn-rank > div > div.text').html();
+}
+function getCharUnique(num) {
+	return $('#char' + num + ' > div.prcn-unique > div > div.text').html();
+}
+function getlastTime() {
+	var result = $('.edit-area div:last-child').children('input[type="time"]').val();
+	if (result === undefined) {
+		return "01:30";
 	}
+	return result;
+}
+
+// タイムチャートコピー
+function execCopy(string) {
+	var tmp = document.createElement("div");
+	var pre = document.createElement('pre');
+	pre.style.webkitUserSelect = 'auto';
+	pre.style.userSelect = 'auto';
+
+	string = string.replace(/<br>/g, "\r\n");
+	tmp.appendChild(pre).textContent = string;
+	var s = tmp.style;
+	s.position = 'fixed';
+	s.right = '200%';
+
+	document.body.appendChild(tmp);
+	document.getSelection().selectAllChildren(tmp);
+	var result = document.execCommand("copy");
+	document.body.removeChild(tmp);
+
+	return result;
+}
+
+// チーム画像生成
+function createTeamSet() {
+
+	var tmp = document.createElement("div");
+	$(tmp).attr('id', 'imgTmp');
+	document.body.appendChild(tmp);
+	//
+	// var charImg = $('#prcn-char-img').children().clone();
+	// console.log(charImg.html());
+	// var imgUrl = getCharImgUrl("アキノ");
+	//
+	// charImg.css('background-image', 'url(' + imgUrl + ')');
+
+	// charImg.attr("src",imgUrl);
+	// charImg.attr("src",imgUrl);
+	// $('#imgTmp').append(charImg);
+
+	html2canvas(document.body, {
+		onrendered : function(canvas) {
+			$(canvas).attr('id','canvas');
+			document.body.appendChild(canvas);
+		},
+		allowTaint : true,
+		useCORS : true,
+		taintTest : false
+	})
+
+//	// HTML内に画像を表示
+//	html2canvas(document.getElementById("target"), {
+//		onrendered : function(canvas) {
+//			// imgタグのsrcの中に、html2canvasがレンダリングした画像を指定する。
+//			var imgData = canvas.toDataURL();
+//			document.getElementById("result").src = imgData;
+//		}
+//	});
+//
+//	// ボタンを押下した際にダウンロードする画像を作る
+//	html2canvas(document.getElementById("target"), {
+//		onrendered : function(canvas) {
+//			// aタグのhrefにキャプチャ画像のURLを設定
+//			var imgData = canvas.toDataURL();
+//			document.getElementById("ss").href = imgData;
+//		}
+//	});
+
+	// document.body.removeChild(tmp);
+
+}
+// キャラ画像取得
+function getCharImg(charId) {
+
+	return $('#prcn-char-img').children('#' + charId + '').clone();
+
+}
