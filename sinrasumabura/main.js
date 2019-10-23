fc3442148c50e438d916587655fd06e84715794e
ika <ika@DESKTOP-24JR8AH>
2019-10-24 08:04:32 +0900

    追加

--- sinrasumabura/main.js
+++ sinrasumabura/main.js
@@ -1,51 +1,72 @@
+
 function sound(n) {
 	$('.sound-file').get(n).play();
 }
 
 $(document).ready(function() {
 
+	$('.sound-file' + '.sp-bgm').prop('volume', 0.1);
 
+	$('.charBtn').on('click', function(e) {
+		console.log('screen=' + e.screenX + ',' + e.screenY);
 
-
-
-	$('.sound-file' + '.sp-bgm').prop('volume', 0.2);
-	$('.sound-file' + '.sp-bgm').get(0).play();
-
-	$('.charBtn').on('click', function() {
-
-		$(".selected").each(function(){
-		    $(this).removeClass("selected");
+		$(".selected").each(function() {
+			$(this).removeClass("selected");
 		});
-
 		$(this).toggleClass('selected');
-		$(this).effect('shake',{direction: 'left', distance: 5,duration:50},20);
-		$(this).effect('highlight',40);
+		$(this).effect('shake', {
+			direction : 'left',
+			distance : 3,
+			duration : 50
+		}, 15);
+		$(this).effect('highlight', 100);
 
 		$('#charBox').animate({
-			top : 10
-		}, 15).animate({
-			top : 0
-		}, 15);
+			top : 15,
+			'borderColor' : '#eeff00'
+		}, 50).animate({
+			top : 0,
+			'borderColor' : '#000000'
+		}, 50);
+
+		$(".cursor").each(function() {
+			$(this).animate({
+				top : e.pageY-30   ,
+				left:e.pageX-30
+			}, 50);
+		});
+
+		 console.log('screen=' + e.screenX + ',' + e.screenY);
+		  console.log('page=' + e.pageX + ',' + e.pageY);
+		  console.log('client=' + e.clientX + ',' + e.clientY);
+		  console.log('offset=' + e.offsetX + ',' + e.offsetY);
+
 
 	});
 
 	$('.charBtn').hover(function() {
-		$('#charTargetImg').css('background-image', $(this).css('background-image'));
-		$('#charTargetDecoCut > span').html($(this).children('a').html());
+		selectChar($(this));
+
+	}, function() {
+		$(".selected").each(function() {
+			selectChar($(this));
+		});
+	});
+
+	function selectChar(__elm__) {
+		$('#charTargetImg').css('background-image', __elm__.css('background-image'));
+		$('#charTargetDecoCut > span').html(__elm__.children('a').html());
 
 		$('#charTargetImg').css('margin-left', '213px');
 
 		$('#charTargetImg').animate({
 			'marginLeft' : '0px'
-		}, 100);
+		}, 50);
 
 		$('#charTargetDecoCut > span').css('margin-left', '20px');
 		$('#charTargetDecoCut > span').animate({
 			'marginLeft' : '0px'
 		}, 100);
-
-	}, function() {
-
-	});
+	}
 
 });
