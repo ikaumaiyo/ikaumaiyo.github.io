5087d3a06b84565b0e83ea942ac3df9c5152487f
ika <ika@DESKTOP-24JR8AH>
2020-02-26 03:35:51 +0900

    up

--- battleLogMoniter/js/analysis.js
+++ battleLogMoniter/js/analysis.js
@@ -474,9 +474,9 @@ class Analysis{
 			// 凸傾向
 			let _nowloc = that.report[that.report.length-1].周回;
 			let keikoTargetWrap = optionList.w1_start_wrap;
-			keikoTargetWrap = _nowloc >= optionList.w4_start_wrap ? optionList.w4_start_wrap : keikoTargetWrap;
-			keikoTargetWrap = _nowloc >= optionList.w3_start_wrap ? optionList.w3_start_wrap : keikoTargetWrap;
 			keikoTargetWrap = _nowloc >= optionList.w2_start_wrap ? optionList.w2_start_wrap : keikoTargetWrap;
+			keikoTargetWrap = _nowloc >= optionList.w3_start_wrap ? optionList.w3_start_wrap : keikoTargetWrap;
+			keikoTargetWrap = _nowloc >= optionList.w4_start_wrap ? optionList.w4_start_wrap : keikoTargetWrap;
 
 			let kisiTotuOver20MReport = that.report.filter(function(_item, _index){
 				if(_item.プリコネーム == val.プリコネーム
